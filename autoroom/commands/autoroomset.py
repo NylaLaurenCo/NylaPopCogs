4bf601550063bca4a4af43fd046f52eff09810ea
The Nyla Lauren Company <61096970+NylaLaurenCo@users.noreply.github.com>
2020-11-21 23:44:13 -0500

    Update autoroomset.py

--- autoroom/commands/autoroomset.py
+++ autoroom/commands/autoroomset.py
@@ -1,4 +1,4 @@
-"""The autoroomset command."""
+"""The admin room settings command."""
 from abc import ABC
 
 import discord
@@ -10,24 +10,24 @@ from ..pcx_lib import SettingDisplay, checkmark
 
 
 class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
-    """The autoroomset command."""
+    """The admin room settings command."""
 
     @commands.group()
     @commands.guild_only()
     @checks.admin_or_permissions(manage_guild=True)
-    async def autoroomset(self, ctx: commands.Context):
-        """Configure the AutoRoom cog."""
+    async def roomset(self, ctx: commands.Context):
+        """Configure private rooms"""
 
-    @autoroomset.command()
+    @roomset.command()
     async def settings(self, ctx: commands.Context):
         """Display current settings."""
         guild_section = SettingDisplay("Guild Settings")
         guild_section.add(
-            "Admin private channel access",
+            "Admin private room access",
             await self.config.guild(ctx.guild).admin_access(),
         )
         guild_section.add(
-            "Moderator private channel access",
+            "Moderator private room access",
             await self.config.guild(ctx.guild).mod_access(),
         )
 
@@ -40,10 +40,10 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
                         avc_settings["dest_category_id"]
                     )
                     autoroom_section = SettingDisplay(
-                        f"AutoRoom - {source_channel.name}"
+                        f"Private Room - {source_channel.name}"
                     )
                     autoroom_section.add(
-                        "Room type",
+                        "Room Access",
                         avc_settings["room_type"].capitalize(),
                     )
                     autoroom_section.add(
@@ -78,38 +78,38 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
 
         await ctx.send(guild_section.display(*autoroom_sections))
 
-    @autoroomset.group()
+    @roomset.group()
     async def access(self, ctx: commands.Context):
-        """Control access to all AutoRooms."""
+        """Control access to all rooms."""
 
     @access.command()
     async def admin(self, ctx: commands.Context):
-        """Allow Admins to join private channels."""
+        """Allow Admins to join private rooms."""
         admin_access = not await self.config.guild(ctx.guild).admin_access()
         await self.config.guild(ctx.guild).admin_access.set(admin_access)
         await ctx.send(
             checkmark(
-                f"Admins are {'now' if admin_access else 'no longer'} able to join (new) private AutoRooms."
+                f"Admins are {'now' if admin_access else 'no longer'} able to join private rooms."
             )
         )
 
     @access.command()
     async def mod(self, ctx: commands.Context):
-        """Allow Moderators to join private channels."""
+        """Allow Moderators to join private roms."""
         mod_access = not await self.config.guild(ctx.guild).mod_access()
         await self.config.guild(ctx.guild).mod_access.set(mod_access)
         await ctx.send(
             checkmark(
-                f"Moderators are {'now' if mod_access else 'no longer'} able to join (new) private AutoRooms."
+                f"Moderators are {'now' if mod_access else 'no longer'} able to join private rooms."
             )
         )
 
-    @autoroomset.group(aliases=["enable", "add"])
+    @roomset.group(aliases=["enable", "add"])
     async def create(self, ctx: commands.Context):
-        """Create an AutoRoom Source.
+        """Create a Room Source.
 
-        Anyone joining an AutoRoom Source will automatically have a new
-        voice channel (AutoRoom) created in the destination category,
+        Anyone joining a Room Source will automatically have a new
+        voice channel (Private Room) created in the destination category 
         and then be moved into it.
         """
 
@@ -120,9 +120,9 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
         source_voice_channel: discord.VoiceChannel,
         dest_category: discord.CategoryChannel,
     ):
-        """Create an AutoRoom Source that creates public AutoRooms.
+        """Create a Room Source for public rooms.
 
-        The created AutoRooms will allow anyone to join. The AutoRoom owner can
+        These rooms will be public by default. The room owner can
         block specific members from joining their room, or can switch the room to
         private mode to selectively allow members instead.
         """
@@ -137,11 +137,10 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
         source_voice_channel: discord.VoiceChannel,
         dest_category: discord.CategoryChannel,
     ):
-        """Create an AutoRoom Source that creates private AutoRooms.
+        """Create an Room Source for private rooms.
 
-        The created AutoRooms will not allow anyone to join. The AutoRoom owner will
-        need to allow specific members to join their room, or can switch the room to
-        public mode to selectively block members instead.
+        The created room will be private by default. The room owner can then allow specific members in or can switch the room to
+        public to allow everyone access.
         """
         await self._create_new_public_private_room(
             ctx, source_voice_channel, dest_category, "private"
@@ -162,8 +161,8 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
             avcs[vc_id]["dest_category_id"] = dest_category.id
         await ctx.send(
             checkmark(
-                "**{}** is now an AutoRoom Source, and will create new {} voice channels in the **{}** category. "
-                "Check out `[p]autoroomset modify` if you'd like to configure this further.".format(
+                "**{}** is now a Room Source, and will create new {} voice channels in the **{}** category. "
+                "Check out `[p]roomset modify` if you'd like to configure this further.".format(
                     source_voice_channel.mention,
                     room_type,
                     dest_category.mention,
@@ -171,13 +170,13 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
             )
         )
 
-    @autoroomset.command(aliases=["disable", "delete", "del"])
+    @roomset.command(aliases=["disable", "delete", "del"])
     async def remove(
         self,
         ctx: commands.Context,
         autoroom_source: discord.VoiceChannel,
     ):
-        """Remove an AutoRoom Source."""
+        """Remove a Room Source."""
         async with self.config.guild(ctx.guild).auto_voice_channels() as avcs:
             try:
                 del avcs[str(autoroom_source.id)]
@@ -185,26 +184,26 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
                 pass
         await ctx.send(
             checkmark(
-                f"**{autoroom_source.mention}** is no longer an AutoRoom Source channel."
+                f"**{autoroom_source.mention}** is no longer a Room Source channel."
             )
         )
 
-    @autoroomset.group(aliased=["edit"])
+    @roomset.group(aliased=["edit"])
     async def modify(self, ctx: commands.Context):
-        """Modify an existing AutoRoom Source."""
+        """Modify an existing Room Source."""
 
     @modify.command(name="public")
     async def modify_public(
         self, ctx: commands.Context, autoroom_source: discord.VoiceChannel
     ):
-        """Set an AutoRoom Source to create public AutoRooms."""
+        """Set a Room Source to create public Rooms."""
         await self._save_public_private(ctx, autoroom_source, "public")
 
     @modify.command(name="private")
     async def modify_private(
         self, ctx: commands.Context, autoroom_source: discord.VoiceChannel
     ):
-        """Set an AutoRoom Source to create private AutoRooms."""
+        """Set a Room Source to create private Rooms."""
         await self._save_public_private(ctx, autoroom_source, "private")
 
     async def _save_public_private(
@@ -220,21 +219,21 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
             except KeyError:
                 await ctx.send(
                     error(
-                        f"**{autoroom_source.mention}** is not an AutoRoom Source channel."
+                        f"**{autoroom_source.mention}** is not a Room Source channel."
                     )
                 )
             else:
                 await ctx.send(
                     checkmark(
-                        f"New AutoRooms created by **{autoroom_source.mention}** will be {room_type}."
+                        f"New Private Rooms created by **{autoroom_source.mention}** will be {room_type}."
                     )
                 )
 
     @modify.group()
     async def memberrole(self, ctx: commands.Context):
-        """Limit AutoRoom visibility to certain member roles.
+        """Limit Room visibility to certain roles.
 
-        When set, only users with the specified role(s) can see AutoRooms.
+        When set, only users with the specified role(s) can see Private Rooms.
         """
 
     @memberrole.command(name="add")
@@ -244,7 +243,7 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
         role: discord.Role,
         autoroom_source: discord.VoiceChannel,
     ):
-        """Add a role to the list of member roles allowed to see these AutoRooms."""
+        """Add a role to the list of member roles allowed to see these Private Rooms."""
         async with self.config.guild(ctx.guild).auto_voice_channels() as avcs:
             try:
                 if "member_roles" not in avcs[str(autoroom_source.id)]:
@@ -254,7 +253,7 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
             except KeyError:
                 await ctx.send(
                     error(
-                        f"**{autoroom_source.mention}** is not an AutoRoom Source channel."
+                        f"**{autoroom_source.mention}** is not a Room Source channel."
                     )
                 )
                 return
@@ -267,7 +266,7 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
         role: discord.Role,
         autoroom_source: discord.VoiceChannel,
     ):
-        """Remove a role from the list of member roles allowed to see these AutoRooms."""
+        """Remove a role from the list of member roles allowed to see these Private Rooms."""
         async with self.config.guild(ctx.guild).auto_voice_channels() as avcs:
             try:
                 if (
@@ -280,7 +279,7 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
             except KeyError:
                 await ctx.send(
                     error(
-                        f"**{autoroom_source.mention}** is not an AutoRoom Source channel."
+                        f"**{autoroom_source.mention}** is not a Room Source channel."
                     )
                 )
                 return
@@ -295,7 +294,7 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
             await ctx.send(
                 checkmark(
                     f"{action}\n"
-                    f"New AutoRooms created by **{autoroom_source.mention}** will be visible by users "
+                    f"New Private Rooms created by **{autoroom_source.mention}** will be visible by members "
                     "with any of the following roles:\n"
                     f"{', '.join([role.mention for role in member_roles])}"
                 )
@@ -304,24 +303,24 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
             await ctx.send(
                 checkmark(
                     f"{action}\n"
-                    f"New AutoRooms created by **{autoroom_source.mention}** will be visible by all users."
+                    f"New Private Rooms created by **{autoroom_source.mention}** will be visible by all members."
                 )
             )
 
     @modify.group()
     async def name(self, ctx: commands.Context):
-        """Set the default name format of an AutoRoom."""
+        """Set the default name format of a Private Rooms."""
 
     @name.command()
     async def username(
         self, ctx: commands.Context, autoroom_source: discord.VoiceChannel
     ):
-        """Default format: PhasecoreX's Room."""
+        """Default format: Hatsumi's Room."""
         await self._save_room_name(ctx, autoroom_source, "username")
 
     @name.command()
     async def game(self, ctx: commands.Context, autoroom_source: discord.VoiceChannel):
-        """The users current playing game, otherwise the username format."""
+        """The name of a game the room owner is currently playing, otherwise the username format."""
         await self._save_room_name(ctx, autoroom_source, "game")
 
     async def _save_room_name(
@@ -337,13 +336,13 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
             except KeyError:
                 await ctx.send(
                     error(
-                        f"**{autoroom_source.mention}** is not an AutoRoom Source channel."
+                        f"**{autoroom_source.mention}** is not a Room Source channel."
                     )
                 )
             else:
                 await ctx.send(
                     checkmark(
-                        f"New AutoRooms created by **{autoroom_source.mention}** "
+                        f"New Private Rooms created by **{autoroom_source.mention}** "
                         f"will use the **{room_type.capitalize()}** format."
                     )
                 )
@@ -361,7 +360,7 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
             except KeyError:
                 await ctx.send(
                     error(
-                        f"**{autoroom_source.mention}** is not an AutoRoom Source channel."
+                        f"**{autoroom_source.mention}** is not a Room Source channel."
                     )
                 )
                 return
@@ -371,7 +370,7 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
                 settings["text_channel"] = True
             await ctx.send(
                 checkmark(
-                    f"New AutoRooms created by **{autoroom_source.mention}** will "
+                    f"New Private Rooms created by **{autoroom_source.mention}** will "
                     f"{'now' if 'text_channel' in settings else 'no longer'} get their own text channel."
                 )
             )
@@ -384,11 +383,11 @@ class AutoRoomSetCommands(MixinMeta, ABC, metaclass=CompositeMetaClass):
         """Learn how to modify default permissions."""
         await ctx.send(
             info(
-                "Any permissions set for the `@everyone` role on an AutoRoom Source will be copied to the "
-                "resulting AutoRoom. The only two permissions that will be overwritten are **View Channel** "
-                "and **Connect**, which depend on the AutoRoom Sources public/private setting, as well as "
+                "Any permissions set for the `@everyone` role on a Room Source will be copied to the "
+                "resulting Private Rooms. The only two permissions that will be overwritten are **View Channel** "
+                "and **Connect**, which depend on the Room Sources public/private setting, as well as "
                 "any member roles enabled for it.\n\n"
-                "Do note that you don't need to set any permissions on the AutoRoom Source channel for this "
+                "Do note that you don't need to set any permissions on the Room Source channel for this "
                 "cog to work correctly. This functionality is for the advanced user with a complex server "
                 "structure, or for users that want to selectively enable/disable certain functionality "
                 "(e.g. video, voice activity/PTT, invites) in AutoRooms."
