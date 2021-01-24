944c6fc71e11fe2a01de56093cbbcd8a7f51e593
The Nyla Lauren Company <61096970+NylaLaurenCo@users.noreply.github.com>
2021-01-24 17:27:06 -0500

    Delete disboardreminder directory

--- disboardreminder/__init__.py
+++
@@ -1,7 +0,0 @@
-from .disboardreminder import DisboardReminder
-
-__red_end_user_data_statement__ = "This cog stores the number of times a member has bumped."
-
-
-def setup(bot):
-    bot.add_cog(DisboardReminder(bot))
