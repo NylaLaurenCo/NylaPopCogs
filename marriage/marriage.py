f3ad3c952fd6cebe910a7f63fd150fc0a870b41d
The Nyla Lauren Company <61096970+NylaLaurenCo@users.noreply.github.com>
2021-01-27 02:27:02 -0500

    Update marriage.py

--- marriage/marriage.py
+++ marriage/marriage.py
@@ -216,8 +216,8 @@ class Marriage(commands.Cog):
             return await ctx.send("Um happiness has to be 100 or less.")
         action = await self.config.guild(ctx.guild).stuff.get_raw(action)
         action[0] = happiness
+        happiness = action[0]
         await self.config.guild(ctx.guild).stuff.set_raw(action, value=[action[0], happiness])
-        action[0] = happiness
         await ctx.tick()
 
     @marriage.command(name="changeprice")
