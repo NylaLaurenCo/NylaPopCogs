bb582054df5e80dd302b7a0abf35c9ae2e3ac12e
The Nyla Lauren Company <61096970+NylaLaurenCo@users.noreply.github.com>
2021-01-30 15:40:19 -0500

    Update mcdonalds.py

--- mcdonalds/mcdonalds.py
+++ mcdonalds/mcdonalds.py
@@ -83,7 +83,7 @@ class McDonalds(Cog):
                          "{}, your shift has ended. You earned **$humanize_number({}) {}** for a hard day's work!".format(
                               ctx.author.display_name, reward, await bank.get_currency_name(ctx.guild)
                          )
-               if reward <= 0:
+               if reward = 0:
                     await ctx.send(
                          "{}, your shift has ended. You might want to look into finding another job, bro.".format(ctx.author.display_name)
                     )
