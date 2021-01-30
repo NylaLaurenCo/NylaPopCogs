6a086ad2923298603c2ff498948f69bf9acfd5d0
The Nyla Lauren Company <61096970+NylaLaurenCo@users.noreply.github.com>
2021-01-30 15:18:41 -0500

    Update recyclingplant.py

--- recyclingplant/recyclingplant.py
+++ recyclingplant/recyclingplant.py
@@ -5,10 +5,10 @@ import random
 from redbot.core import bank, commands
 from redbot.core.commands import Cog
 from redbot.core.data_manager import bundled_data_path
+from redbot.core.utils.chat_formatting import humanize_number, humanize_timedelta, pagify
 
-
-class RecyclingPlant(Cog):
-    """Apply for a job at the recycling plant!"""
+class McDonalds(Cog):
+    """Pick up a shift at McDonald's!"""
 
     def __init__(self, bot):
         super().__init__()
@@ -24,27 +24,27 @@ class RecyclingPlant(Cog):
         with junk_path.open() as json_data:
             self.junk = json.load(json_data)
 
-    @commands.command(aliases=["recycle"])
-    async def recyclingplant(self, ctx: commands.Context):
-        """Apply for a job at the recycling plant!"""
+    @commands.command(aliases=["re-serve"])
+    async def mcdonalds(self, ctx: commands.Context):
+        """Pick up a shift at McDonald's!"""
         if self.junk is None:
             self.load_junk()
 
         x = 0
         reward = 0
         await ctx.send(
-            "{0} has signed up for a shift at the Recycling Plant! Type ``exit`` to terminate it early.".format(
+            "<:pepe_jord:804810873570852884> {0} just signed up for a shift at McDonald's! You're job today is sorting the trash.\n:sh_space:\nWhen you've finished your shift, type `end` to clock out.\n:sh_space:\n".format(
                 ctx.author.display_name
             )
         )
         while x in range(0, 10):
             used = random.choice(self.junk["can"])
             if used["action"] == "trash":
-                opp = "recycle"
+                opp = "re-serve"
             else:
                 opp = "trash"
             await ctx.send(
-                "``{}``! Will {} ``trash`` it or ``recycle`` it?".format(
+                "You dug in the trash and found `{}`. {}, will you leave it in the `trash` or `re-serve` it to customers?".format(
                     used["object"], ctx.author.display_name
                 )
             )
@@ -59,37 +59,44 @@ class RecyclingPlant(Cog):
 
             if answer is None:
                 await ctx.send(
-                    "``{}`` fell down the conveyor belt to be sorted again!".format(used["object"])
+                    "`{}` fell on the floor and needs sorting again!".format(used["object"])
                 )
             elif answer.content.lower().strip() == used["action"]:
                 await ctx.send(
-                    "Congratulations! You put ``{}`` down the correct chute! (**+50**)".format(
-                        used["object"]
+                    "<:cash:803730921785524234> Good job! Keep this up and you'll be employee of the month! **+$50 {}**".format(
+                        await bank.get_currency_name(ctx.guild)]
                     )
                 )
-                reward += 50
-                x += 1
+                reward =+50
+                x =+ 1
             elif answer.content.lower().strip() == opp:
                 await ctx.send(
-                    "{}, you little brute, you put it down the wrong chute! (**-50**)".format(
-                        ctx.author.display_name
+                    "<:wrong:728806094113210369> {}, you moron! That's not how things work here! I'm docking your pay **$50 {}**!".format(
+                        ctx.author.display_name, await bank.get_currency_name(ctx.guild)
                     )
                 )
-                reward -= 50
-            elif answer.content.lower().strip() == "exit":
-                await ctx.send(
-                    "{} has been relived of their duty.".format(ctx.author.display_name)
-                )
+                reward =-50
+            elif answer.content.lower().strip() == "end":
+               if reward > 0:
+                    await bank.deposit_credits(ctx.author, reward)
+                    await ctx.send(
+                         "{}, your shift has ended. You earned **$humanize_number({}) {}** for a hard day's work!".format(
+                              ctx.author.display_name, reward, await bank.get_currency_name(ctx.guild)
+                         )
+               else:
+                    await ctx.send(
+                         "{}, your shift has ended. You might want to look into finding another job, bro.".format(ctx.author.display_name)
+                    )
                 break
             else:
                 await ctx.send(
-                    "``{}`` fell down the conveyor belt to be sorted again!".format(used["object"])
+                    "`{}` fell on the floor and needs sorting again!".format(used["object"])
                 )
         else:
             if reward > 0:
-                await bank.deposit_credits(ctx.author, reward)
-            await ctx.send(
-                "{} been given **{} {}s** for your services.".format(
-                    ctx.author.display_name, reward, await bank.get_currency_name(ctx.guild)
-                )
+               await bank.deposit_credits(ctx.author, reward)
+               await ctx.send(
+                    "{}, your shift has ended. You earned **$humanize_number({}) {}** for a hard day's work!".format(
+                         ctx.author.display_name, reward, await bank.get_currency_name(ctx.guild)
+                    )
             )
