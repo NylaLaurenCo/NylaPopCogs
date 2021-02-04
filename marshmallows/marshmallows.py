881c37037511248813a471428d8f4a414f8bd329
The Nyla Lauren Company <61096970+NylaLaurenCo@users.noreply.github.com>
2021-02-04 15:48:21 -0500

    Update marshmallows.py

--- marshmallows/marshmallows.py
+++ marshmallows/marshmallows.py
@@ -216,7 +216,7 @@ class Marshmallows(commands.Cog):
     @commands.guild_only()
     async def buymallows(self, ctx: commands.Context, amount: int):
         currency = await bank.get_currency_name(ctx.guild)
-        buycost = str(humanize_number_int(100 / await self.config.guild(ctx.guild).cost())))
+        buycost = str(humanize_number(int(100 / await self.config.guild(ctx.guild).cost())))
         """Buy marshmallows. A 100-piece bag of mallows costs {buycost} {currency}."""
         if amount <= 0:
             return await ctx.send("Are you okay?")
